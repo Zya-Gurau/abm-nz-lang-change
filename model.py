20bc6e1a0db3c9ea6461e13c1c72ab25502adc4a
Zya Gurau <92696912+Zya-Gurau@users.noreply.github.com>
2024-09-11 12:05:59 +1200

    feat - Added threshhold for speaking
    threshhold for if an Agent is considered to be speaking the variant

--- model.py
+++ model.py
@@ -1,44 +1,55 @@
 import math
 import random
 from network_setup import *
-import Agent
+from Agent import *
 from datetime import datetime
 import matplotlib.pyplot as plt
 from numba import jit, cuda
 import numpy as np
 
-#ADD CHILDREN
-def new_generation(agents, lam, h):
+def new_generation(agents, lam, h, set_speaking, prop_speaking, num_child):
+    new_agents = []
+
+    #remove current adults
     for agent in agents:
-        if agent.isAdult == True:
-            #remove current adult
-            agents.remove(agent)
-        else:
-            #make adult
+        if agent.isAdult == False:
+            new_agents.append(agent)
+        #else:
+            #if agent in set_speaking:
+                #set_speaking.remove(agent)
+            
+
+    #make new adults
+    for agent in new_agents:
+        if agent.isAdult == False:
             agent.isAdult = True
             agent.lamb = agent.lamb * (1/200)
-    #create new children
-    for agent in agents:
-        if agent.isAdult == True:
-            new_child = Agent(lam, h, agent.ideolect, 0, False)
-            agents.append(new_child)
-    pass
 
-def agent_interaction(agent_one, agent_two, prop_speaking, set_speaking, weights):
-    global PROPORTION_SPEAKING_A_FIFTY
+    #create new children
+    for agent in new_agents:
+        if agent.isAdult == True:
+            for i in range(num_child):
+                new_child = Agent(lam, h, agent.ideolect, 0, False)
+                new_agents.append(new_child)
+            #if agent in set_speaking:
+                #set_speaking.add(new_child)
+
+    return new_agents, prop_speaking
+
+def agent_interaction(agent_one, agent_two, prop_speaking, set_speaking, weights, THRESHHOLD):
     one_utterance = agent_one.reproduction()
     two_utterance = agent_two.reproduction()
 
     agent_one.retention(one_utterance, two_utterance, agent_two, weights)
 
-    if agent_one.id not in set_speaking and agent_one.ideolect[0] >= 1:
-        set_speaking.add(agent_one.id)
+    if agent_one not in set_speaking and agent_one.ideolect[0] >= THRESHHOLD:
+        set_speaking.add(agent_one)
         prop_speaking += 1
 
     agent_two.retention(two_utterance, one_utterance, agent_one, weights)
 
-    if agent_two.id not in set_speaking and agent_two.ideolect[0] >= 1:
-        set_speaking.add(agent_two.id)
+    if agent_two not in set_speaking and agent_two.ideolect[0] >= THRESHHOLD:
+        set_speaking.add(agent_two)
         prop_speaking += 1
     return prop_speaking
 
@@ -49,29 +60,51 @@ def choose_agents(agents):
     second_agent = agent_list[1]
     return first_agent, second_agent
 
-def setup_model(NUM_AGENTS, INITIAL_VARIANT_OCCURENCE, H, LAMBDA, WEIGHTS, NUM_SIMULATIONS):
+def setup_model(NUM_AGENTS, INITIAL_VARIANT_OCCURENCE, H, LAMBDA, WEIGHTS, NUM_SIMULATIONS, USE_GEN_REPLACE, NUM_CHILD_PER_ADULT, THRESHHOLD):
     set_x = []
     set_y = []
     max_x = set()
     for i in range(0, NUM_SIMULATIONS):
+        num_interactions = 0
+        finish = 0
         x = []
         y = []
         cur_x = 0
-        prop_speaking = math.floor(NUM_AGENTS * INITIAL_VARIANT_OCCURENCE)
-        agents, set_speaking = get_model_agents(H, LAMBDA, NUM_AGENTS, INITIAL_VARIANT_OCCURENCE)
-        while prop_speaking < NUM_AGENTS:
-            
+        
+        if USE_GEN_REPLACE == True:
+            prop_speaking = (NUM_CHILD_PER_ADULT + 1) *math.floor(NUM_AGENTS * INITIAL_VARIANT_OCCURENCE)
+            finish = (NUM_CHILD_PER_ADULT + 1) * NUM_AGENTS
+        else:
+            prop_speaking = math.floor(NUM_AGENTS * INITIAL_VARIANT_OCCURENCE)
+            finish = NUM_AGENTS
+
+        agents, set_speaking = get_model_agents(H, LAMBDA, NUM_AGENTS, INITIAL_VARIANT_OCCURENCE, USE_GEN_REPLACE, NUM_CHILD_PER_ADULT)
+        
+        while len(set_speaking) < len(agents):
+
+            if USE_GEN_REPLACE:
+                num_interactions += 1
+                if num_interactions % ((1.3 * (10 ** 5)) * ((NUM_CHILD_PER_ADULT + 1) * NUM_AGENTS) /25 ) == 0:
+                    
+                    agents, prop_speaking = new_generation(agents, LAMBDA, H, set_speaking, prop_speaking, NUM_CHILD_PER_ADULT)
+                    print("NEW GENERATION!!!")
+                    print("Number of interactions: " + str(num_interactions))
+
             agent_one, agent_two = choose_agents(agents)
-            prop_speaking = agent_interaction(agent_one, agent_two, prop_speaking, set_speaking, WEIGHTS)
+            prop_speaking = agent_interaction(agent_one, agent_two, prop_speaking, set_speaking, WEIGHTS, THRESHHOLD)
             x.append(cur_x)
-            y.append(prop_speaking)
+            y.append(len(set_speaking))
             max_x.add(cur_x)
             cur_x+=1
+
+        print("SIM COMPLETE")
         set_x.append(x)
         set_y.append(y)
+
     mean_x_axis = [i for i in range(max(max_x))]
     ys_interp = [np.interp(mean_x_axis, set_x[i], set_y[i]) for i in range(len(set_x))]
     mean_y_axis = np.mean(ys_interp, axis=0)
+
     for i in range(0, len(set_x)):
         plt.plot(set_x[i], set_y[i])
     plt.plot(mean_x_axis, mean_y_axis)
--- network_setup.py
+++ network_setup.py
@@ -2,30 +2,40 @@ import math
 from Agent import *
 import numpy as np
 
-def get_model_agents(h, lam, num, init):
+def get_model_agents(h, lam, num, init, use_gen, num_child):
     agents = []
     set_speaking = set()
-
-    #adults
-    for i in range(0, math.floor(num*init)):
-        agent = Agent(lam * (1/200), h, [1, 0], i, True)
-        agents.append(agent)
-        set_speaking.add(agent.id)
-
-    for i in range(math.floor(num*init), num):
-        agent = Agent(lam * (1/200), h, [0, 1], i, True)
-        agents.append(agent)
-
-    #children
-    for i in range(0, math.floor(num*init)):
-        agent = Agent(lam, h, [1, 0], i, False)
-        agents.append(agent)
-        set_speaking.add(agent.id)
-
-    for i in range(math.floor(num*init), num):
-        agent = Agent(lam, h, [0, 1], i, False)
-        agents.append(agent)
-        
     
+    if use_gen == True:
+        #Adults
+        for i in range(0, math.floor(num*init)):
+            agent = Agent(lam * (1/200), h, [1, 0], i, True)
+            agents.append(agent)
+            set_speaking.add(agent)
+
+        for i in range(math.floor(num*init), num):
+            agent = Agent(lam * (1/200), h, [0, 1], i, True)
+            agents.append(agent)
+
+        #children
+        for i in range(0, math.floor(num*init)*num_child):
+            agent = Agent(lam, h, [1, 0], num+i, False)
+            agents.append(agent)
+            set_speaking.add(agent)
+
+        for i in range(math.floor(num*init)*num_child, num*num_child):
+            agent = Agent(lam, h, [0, 1], num+i, False)
+            agents.append(agent)
+    else:
+        #children
+        for i in range(0, math.floor(num*init)):
+            agent = Agent(lam, h, [1, 0], num+i, False)
+            agents.append(agent)
+            set_speaking.add(agent)
+
+        for i in range(math.floor(num*init), num):
+            agent = Agent(lam, h, [0, 1], num+i, False)
+            agents.append(agent)
+        
     return agents, set_speaking
 
